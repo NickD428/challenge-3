dc51238fd7be34b76f4b8abaced59876a59ccfaa
Nick <chargerfan428@aol.com>
2023-04-18 20:51:22 -0700

    added extra variable and password parameters

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,12 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const letters = "abcdefghijklmnopqrstuvwxyz";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ;"
 const numbers = "1234567890";
-const symbols = "!?@#$%^&*-+=";
+const special = "!?@#$%^&*-+=";
+const all = lowerCase + upperCase + numbers + special;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(Math.random(all));
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
